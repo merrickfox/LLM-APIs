bbde96904636e1e7df7caf3105ff1cd67285e83b
Merrickfox <merrick.fox@gmail.com>
2023-07-10 11:22:13 +0100

    refine server

--- guidance-cloud-api/guidance-broker/guidance.py
+++ guidance-cloud-api/guidance-broker/guidance.py
@@ -1,16 +1,23 @@
 from flask import Flask, request, jsonify
+import guidance
+
+guidance.llm = guidance.llms.TGWUI("http://127.0.0.1:9555")
 
 app = Flask(__name__)
 
-@app.route('/guidance', methods=['POST'])
+@app.route('/guidance-api', methods=['POST'])
 def guidance():
     data = request.get_json()
 
     args = data.get('args', {})
     payload = data.get('payload', "")
 
+    execute_prompt = guidance(payload)
+
+    res = execute_prompt(**args)
+
     # use args and payload as needed
-    return jsonify({"message": "Received", "args": args, "payload": payload})
+    return jsonify({"data": res})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8181)
